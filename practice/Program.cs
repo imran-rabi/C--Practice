7b2a9006549772d59e27d5869989b4ed91157e5a
imran-rabi <kidpalestine2023@gmail.com>
2024-10-13 19:28:37 -0500

    RPS Game in C#

---
+++ practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInput userInput = new UserInput();
+            userInput.GetUserInput();
+        }
+    }
+
+    class UserInput
+    {
+        public void GetUserInput()
+        {
+            Console.WriteLine("Please Input your name:");
+            string userName = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Hello " + userName + "!");
+        }
+    }
+}
+
+
+
